8ed4d405788e41f41f1e422c9f78c2d464a637e8
Todd Burry <todd@vanillaforums.com>
2016-04-30 07:52:08 -0400

    Fix typo

--- applications/dashboard/models/class.usermodel.php
+++ applications/dashboard/models/class.usermodel.php
@@ -1078,7 +1078,7 @@ class UserModel extends Gdn_Model {
      */
     public function definePermissions($UserID, $Serialize = false) {
         if ($Serialize) {
-            trigger_error("UserModel->definePermissions(id, true)", "UserModel->definePermissions(id)");
+            deprecated("UserModel->definePermissions(id, true)", "UserModel->definePermissions(id)");
         }
 
         if (Gdn::cache()->activeEnabled()) {
